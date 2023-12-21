a1c3d708c076fb39b2cbc3ca74b2191fe9a97188
bisriastro <bisri.fadillah@astronauts.id>
2023-12-21 16:39:54 +0700

    add event unit tests

--- app/src/test/java/com/bisri/id/research/compose/circuit/CounterDetailPresenterTest.kt
+++ app/src/test/java/com/bisri/id/research/compose/circuit/CounterDetailPresenterTest.kt
@@ -1,5 +1,6 @@
 package com.bisri.id.research.compose.circuit
 
+import com.bisri.id.research.compose.circuit.counter.CounterScreen
 import com.bisri.id.research.compose.circuit.counterdetail.CounterDetailPresenter
 import com.bisri.id.research.compose.circuit.counterdetail.CounterDetailScreen
 import com.slack.circuit.test.FakeNavigator
@@ -8,6 +9,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThat
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -29,6 +31,58 @@ class CounterDetailPresenterTest {
                 assertEquals(CounterDetailScreen.State.Loading, awaitItem())
                 val result = awaitItem() as CounterDetailScreen.State.Success
                 assertEquals(1, result.id)
+            }
+        }
+    }
+
+    @Test
+    fun `navigate to counter screen`() {
+        val presenter = CounterDetailPresenter(
+            screen = CounterDetailScreen(1),
+            navigator = navigator
+        )
+
+        runTest {
+            presenter.test {
+                assertEquals(CounterDetailScreen.State.Loading, awaitItem())
+                val result = awaitItem() as CounterDetailScreen.State.Success
+                assertEquals(1, result.id)
+
+                val pop = CounterDetailScreen.Event.Pop
+                result.event(pop)
+
+                val nextScreen = navigator.awaitNextScreen()
+                assertEquals(true, nextScreen is ComposePopNavigationScreen)
+                assertEquals(
+                    true,
+                    (nextScreen as ComposePopNavigationScreen).bundle["isResultReceived"]
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `navigate to fragment screen`() {
+        val presenter = CounterDetailPresenter(
+            screen = CounterDetailScreen(1),
+            navigator = navigator
+        )
+
+        runTest {
+            presenter.test {
+                assertEquals(CounterDetailScreen.State.Loading, awaitItem())
+                val result = awaitItem() as CounterDetailScreen.State.Success
+                assertEquals(1, result.id)
+
+                val goTo = CounterDetailScreen.Event.GoTo
+                result.event(goTo)
+
+                val nextScreen = navigator.awaitNextScreen()
+                assertEquals(true, nextScreen is FragmentNavigationScreen)
+                assertEquals(
+                    "circuit://screen/first",
+                    (nextScreen as FragmentNavigationScreen).url
+                )
             }
         }
     }
